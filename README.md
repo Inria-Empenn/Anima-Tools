8c35bdfe1b0b48c4ee397bb5aceae89c56c10e4b
Emmanuel Caruyer <caruyer@gmail.com>
2025-02-04 13:57:51 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Anima-Tools
-A collection of tools, written in Python, to help with data conversion from/to Anima-compatible version
+A collection of tools, written in Python, to help with data conversion from/to
+Anima-compatible version
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="animatools",
+    version="0.1",
+    description="A collection of tools to help with data conversion from/to "
+                "Anima-compatible version",
+    author="Emmanuel Caruyer",
+    author_email='Emmanuel.Caruyer@irisa.fr',
+    platforms=["any"],
+    license="",
+    packages=find_packages(),
+    install_requires=[],
+)
